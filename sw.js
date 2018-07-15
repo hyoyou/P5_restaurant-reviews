752289973efdee271d6e4e44bbbd2a6767502f3a
Heather You <heatherhyou@gmail.com>
2018-07-14 22:35:03 -0400

    add ignoreSearch to service worker to ignore query

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ self.addEventListener("install", function(event) {
   
 self.addEventListener("fetch", function(event) {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
+        caches.match(event.request, {ignoreSearch: true}).then(function(response) {
             return response || fetch(event.request);
         })
     );
